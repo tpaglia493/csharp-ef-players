aec18599afc817306bc648db6d71f8f1d81dfd17
tpaglia493 <tommasopaglia93@outlook.it>
2023-05-10 16:50:50 +0200

    milestone 1 - added instance for Player object in main program and method to add to database - nedd to make setters for numberofmatches ecc..

--- csharp-ef-players/Program.cs
+++ csharp-ef-players/Program.cs
@@ -1,45 +1,44 @@
-﻿bool go = true;
-while (go)
-{
-    Console.WriteLine("------- MENU -------\n");
-    Console.WriteLine("1. Insert a new player");
-    Console.WriteLine("2. Insert a team");
-    Console.WriteLine("3. Esci");
-    Console.WriteLine("\n-------------------");
+﻿using System;
 
-    Console.Write("Choose an option: ");
+bool go = true;   
+Console.WriteLine("------- MENU -------\n");
+Console.WriteLine("1. Insert a new player");
+Console.WriteLine("2. Insert a team");
+Console.WriteLine("3. Menu");
+Console.WriteLine("4. Esci");
+Console.WriteLine("\n-------------------");
+while (go) { 
+ 
+
+    Console.Write("\nChoose an option: ");
     int response = int.Parse(Console.ReadLine());
-    switch (response)
-    {
-        case 1:
-            Console.Write("Inserisci il nome della software house: ");
-            string nameSoftwareHouse = Console.ReadLine();
-​
-            Console.WriteLine("Inserisci la partita IVA : ");
-            string vatNumber = Console.ReadLine();
-​
-            Console.WriteLine("Inserisci la descrizione della software house : ");
-            string softwareHouseDescription = Console.ReadLine();
-​
-            
+    switch (response) 
+    { 
+            case 1:
+            Console.Write("Insert the name of the player: ");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("Insert the surname : ");
+            string playerSurname = Console.ReadLine();
             break;
-​
+
         case 2:
-            Console.WriteLine("Inserisci il nome : ");
-            string name = Console.ReadLine();
-​
-            Console.WriteLine("Inserisci la data di pubblicazione (yyyy-mm-dd) : ");
-            DateTime releaseDate = DateTime.Parse(Console.ReadLine());
-​
-            Console.WriteLine("Inserisci l'id della software house che lo ha prodotto: ");
-            int softwareHouseId = int.Parse(Console.ReadLine());
-​
+            Console.WriteLine(" - Still working on it\n - we'll keep you updated ");
             break;
 
         case 3:
-            Console.WriteLine("Grazie e arrivederci!");
+            Console.WriteLine("------- MENU -------\n");
+            Console.WriteLine("1. Insert a new player");
+            Console.WriteLine("2. Insert a team");
+            Console.WriteLine("3. Menu");
+            Console.WriteLine("4. Esci");
+            Console.WriteLine("\n-------------------\n");
+            break;
+
+        case 4:
+            Console.WriteLine("thank you and goodbye");
             go = false;
             break;
+
         default:
             Console.WriteLine("Non hai inserito un'opzione valida! Ritenta!");
             break;
