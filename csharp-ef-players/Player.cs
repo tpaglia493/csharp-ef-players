6d57f33c8cd80f0a623b8c6e4e374f9984b2a1c2
tpaglia493 <tommasopaglia93@outlook.it>
2023-05-10 15:09:44 +0200

    Milestone 0 - added data annotations and constructor to Player class

--- csharp-ef-players/Player.cs
+++ csharp-ef-players/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,24 @@ namespace csharp_ef_players
     internal class Player
     {
         //PROPERTIES
+        [Key]
         public int Id {  get; set; }
+        [MaxLength(50)]
         public string Name { get; set; }
+        [MaxLength(50)]
         public string Surname { get; set; }
+
         public byte Score { get; set; }
         public ushort NumberOfPlayedMatches { get; set; }
         public ushort NumberOfVictories { get; set; }
 
 
         //CONSTRUCTOR
+        public Player(int id, string name, string surname) 
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+        }
     }
 }
