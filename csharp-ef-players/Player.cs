f7a018f5ebfaf95a40923e39e94ec26d71eac895
tpaglia493 <tommasopaglia93@outlook.it>
2023-05-10 14:41:41 +0200

    MIlestone 0 - setup Player class

---
+++ csharp-ef-players/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ef_players
+{
+    internal class Player
+    {
+        //PROPERTIES
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public byte Score { get; set; }
+        public ushort NumberOfPlayedMatches { get; set; }
+        public ushort NumberOfVictories { get; set; }
+
+
+        //CONSTRUCTOR
+    }
+}
---
+++ csharp-ef-players/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ef_players
+{
+    internal class Team
+    {
+        //PROPERTIES
+
+        //CONSTRUCTOR
+    }
+}
